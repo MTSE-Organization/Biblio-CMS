20f73e1c4e905995c18b03fd7ee5c26f36b326a2
LeTanTru <letantru@gmail.com>
2025-09-11 23:35:39 +0700

    feat: update check permission

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,5 @@
 import { apiConfig } from '@/constants';
 
-type RouteConfig = {
-  path: string;
-  permissionCode?: string[];
-  ignoredAuth?: boolean;
-  children?: Record<string, RouteConfig>;
-};
-
 const defineRoute = <T extends Record<string, any>>(routes: T): T => routes;
 
 const route = defineRoute({
@@ -16,6 +9,7 @@ const route = defineRoute({
   account: {
     getList: {
       path: '/account',
+      auth: true,
       permissionCode: [apiConfig.account.getList.permissionCode]
     }
   },
@@ -25,10 +19,12 @@ const route = defineRoute({
   group: {
     getList: {
       path: '/group-permission',
+      auth: true,
       permissionCode: [apiConfig.group.getList.permissionCode]
     },
     savePage: {
       path: '/group-permission/:id',
+      auth: true,
       permissionCode: [
         apiConfig.group.create.permissionCode,
         apiConfig.group.update.permissionCode
@@ -37,21 +33,23 @@ const route = defineRoute({
   },
   login: {
     path: '/login',
-    ignoredAuth: true
+    auth: false
   },
   profile: {
     savePage: {
       path: '/profile',
-      permissionCode: [apiConfig.account.updateProfile.permissionCode]
+      auth: true
     }
   },
   category: {
     getList: {
       path: '/category',
+      auth: true,
       permissionCode: [apiConfig.category.getList.permissionCode]
     },
     savePage: {
       path: '/category/:id',
+      auth: true,
       permissionCode: [
         apiConfig.category.create.permissionCode,
         apiConfig.category.update.permissionCode
@@ -61,10 +59,12 @@ const route = defineRoute({
   product: {
     getList: {
       path: '/product',
+      auth: true,
       permissionCode: [apiConfig.product.getList.permissionCode]
     },
     savePage: {
       path: '/product/:id',
+      auth: true,
       permissionCode: [
         apiConfig.product.create.permissionCode,
         apiConfig.product.update.permissionCode
