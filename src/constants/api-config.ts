aef560c5d3fe5179a41bc27d43dccaaecdd2d459
LeTanTru <letantru@gmail.com>
2025-09-12 14:57:10 +0700

    feat: update permission code

--- src/constants/api-config.ts
+++ src/constants/api-config.ts
@@ -130,7 +130,7 @@ const apiConfig = defineApiConfig({
       baseUrl: `${AppConstants.apiUrl}v1/permission-group/list`,
       method: 'GET',
       headers: baseHeader,
-      permissionCode: 'PER_GR_L'
+      permissionCode: 'PER_GR_L1'
     },
     create: {
       baseUrl: `${AppConstants.apiUrl}v1/permission-group/create`,
@@ -162,7 +162,7 @@ const apiConfig = defineApiConfig({
       baseUrl: `${AppConstants.apiUrl}v1/category/list`,
       method: 'GET',
       headers: baseHeader,
-      permissionCode: 'CAT_L1'
+      permissionCode: 'CAT_L'
     },
     create: {
       baseUrl: `${AppConstants.apiUrl}v1/category/create`,
